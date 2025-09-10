b30b2b3a5b369edfb34d8a38e05c6ee5a22f0f5d
Bikael <mmacdoug02@gmail.com>
2025-09-10 15:02:54 -0400

    toggle visibility of div

--- content.js
+++ content.js
@@ -1,3 +1,24 @@
 const mySideBar = document.createElement('div');
 mySideBar.className = 'custom-sidebar';
 document.body.appendChild(mySideBar);
+
+
+
+function toggleSideBar() {
+    console.log("in toggleSideBar");
+    console.log(mySideBar.style.display);
+    if (mySideBar.style.display == 'none') {
+        mySideBar.style.display = 'flex';
+    } else{
+        console.log("in here");
+        mySideBar.style.display = 'none';
+    }
+}
+
+function handleKeyPress(event) {
+    if (event.shiftKey && event.ctrlKey && event.key.toLowerCase() === 'm') {
+        console.log("shortcut was pressed");
+        toggleSideBar();
+    }
+}
+document.addEventListener('keydown', handleKeyPress);
